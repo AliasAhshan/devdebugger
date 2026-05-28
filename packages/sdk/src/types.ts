c3752e6520b888816adb31d027a31ac20d3f6da4
AliasAhshan <ahshan.ahmed003@gmail.com>
2026-05-28 17:25:47 -0400

    Created initial interpretation of types.ts for sdk

---
+++ packages/sdk/src/types.ts
@@ -0,0 +1,30 @@
+export type DevDebuggerConfig = {
+  projectKey: string;
+  enabled: boolean;
+  endpoint?: string;
+  captureConsoleErrors?: boolean;
+  captureRuntimeErrors?: boolean;
+  capturePromiseErrors?: boolean;
+  showOverlay?: boolean;
+};
+
+export type DevDebuggerEventType =
+  | 'window_error'
+  | 'console_error'
+  | 'react_error'
+  | 'unhandled_rejection';
+
+export type DevDebuggerErrorPayload = {
+  type: DevDebuggerEventType;
+  message: string;
+  url: string;
+  environmentInfo: string;
+  timestamp: string;
+  stack?: string;
+  errorSource?: string;
+  line?: number;
+};
+
+export type DevDebuggerListenerCallback = (
+  payload: DevDebuggerErrorPayload,
+) => void;
